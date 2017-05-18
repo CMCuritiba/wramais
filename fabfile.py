8ef84669d783f759f163edb9071184b0d39beafa
Alexandre Odoni <alexandre.odoni@cmc.pr.gov.br>
2017-05-18 11:40:17 -0300

    correcoes no arquivo de deploy fabric. deploy testado com sucesso em servidor virtual rodando debian 9

--- fabfile.py
+++ fabfile.py
@@ -6,7 +6,8 @@ import os
 
 
 PROJECT_NAME = 'wramais'
-PROJECT_ROOT = '/usr/share/webapps/%s' % PROJECT_NAME
+WEBAPPS = '/usr/share/webapps'
+PROJECT_ROOT = WEBAPPS + '/%s' % PROJECT_NAME
 REPO = 'git@gitlab.cmc.pr.gov.br:desenv/wramais.git'
 
 env.hosts = []
@@ -20,13 +21,13 @@ def localhost():
 
 @task
 def staging():
-	env.hosts = ['telefonia.cmc.pr.gov.br']
+	env.hosts = ['192.168.56.101']
 	env.environment = 'staging'	
-	env.user = 'suporte'
+	env.user = 'django'
 	env.virtualenv = '/usr/share/envs/wramais'
 	env.activate = 'source /usr/share/envs/wramais/bin/activate'
-	env.wwwdata = 'suporte'
-	env.python_location = '/usr/bin/python3.4'
+	env.wwwdata = 'www-data'
+	env.python_location = '/usr/bin/python3.5'
 
 @task
 def production():
@@ -45,6 +46,10 @@ def clean():
 def chown():
 	''' Seta permissões ao usuário/grupo corretos '''
 	sudo('chown -R {}:{} {}'.format(env.wwwdata, env.wwwdata, PROJECT_ROOT))
+
+def cria_webapps():
+	sudo('mkdir -p {}'.format(WEBAPPS))
+	sudo('chown -R {}:{} {}'.format(env.user, env.wwwdata, WEBAPPS))
 
 def restart():
 	sudo('supervisorctl reread')
@@ -90,31 +95,49 @@ def install_production():
 
 @task
 def bootstrap():
-	''' Atualiza código para o servidor de aplicação '''
+	# Atualiza código para o servidor de aplicação
 
-	''' Este comando instala todos os pacotes necessários no servidor '''
-	'''
-	sudo('apt-get update && apt-get install git supervisor nginx memcached libjpeg8-dev postgresql libpq-dev python-dev python-pip python-virtualenv libfreetype6-dev libncurses5-dev libxml2-dev libxslt1-dev zlib1g-dev')
-	'''
+	# Este comando instala todos os pacotes necessários no servidor
+	
+	#sudo('apt-get update && apt-get install git supervisor nginx memcached libjpeg8-dev postgresql libpq-dev python-dev python-pip python-virtualenv libfreetype6-dev libncurses5-dev libxml2-dev libxslt1-dev zlib1g-dev')
+	sudo('apt-get update')
+	sudo('apt-get install git')
+	sudo('apt-get install supervisor')
+	sudo('apt-get install nginx')
+	sudo('apt-get install memcached')
+	sudo('apt-get install libpq-dev')
+	sudo('apt-get install python-dev')
+	sudo('apt-get install python3.5-dev')
+	sudo('apt-get install python-pip')
+	sudo('apt-get install python-virtualenv')
+	sudo('apt-get install libfreetype6-dev')
+	sudo('apt-get install libncurses5-dev')
+	sudo('apt-get install libxml2-dev')
+	sudo('apt-get install libxslt1-dev')
+	sudo('apt-get install zlib1g-dev')
+	sudo('apt-get install libffi-dev')
+	sudo('apt-get install libsasl2-dev')
+	sudo('apt-get install libldap2-dev')
+	sudo('apt-get install libssl-dev')
 
-	''' Cria os diretórios e permissões necessários '''
-	sudo('mkdir -p {}'.format(PROJECT_ROOT))
-	chown()
+	# Cria os diretórios e permissões necessários 
+	cria_webapps()	
+	run('mkdir -p {}'.format(PROJECT_ROOT))
 	run('git clone {} {}'.format(REPO, PROJECT_ROOT))
 
 	with cd(PROJECT_ROOT):
-		''' Atualiza servidor com última versão do master '''
+		# Atualiza servidor com última versão do master
 		run('git pull origin master')
 
-		''' Seta as permissões para todos os arquivos do diretório '''
-		chown()
-	
-		''' Cria o ambiente virtual do projeto '''
+		# Cria o ambiente virtual do projeto 
 		sudo('virtualenv --python={} {}'.format(env.python_location, env.virtualenv))
 
 		with source_virtualenv():
-			''' Ativa o ambiente virtual '''
+			# Ativa o ambiente virtual 
 			run(env.activate)
 
-			''' Instala todos os pacotes no servidor '''
+			# Instala todos os pacotes no servidor 
 			sudo('pip install -r requirements.txt')
+
+	# Acerta o usuário/grupo
+	chown()
